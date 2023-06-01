19c644126aaa248dd195fc5396590175d9512025
Armen Martirosian <amartirosyan08@yandex.ru>
2023-06-01 20:22:39 +0400

    Выполненное задание 7-го спринта

---
+++ src/main/java/config/BaseURI.java
@@ -0,0 +1,6 @@
+package config;
+
+public class BaseURI {
+    public static final String baseURI = "http://qa-scooter.praktikum-services.ru/";
+}
+
---
+++ src/main/java/data/CourierData.java
@@ -0,0 +1,20 @@
+package data;
+
+import model.Courier;
+
+public class CourierData {
+
+    public static Courier defaultCourier(){
+        return new Courier("murad_legendaaue", "Feduk_one_love", "Интересно");
+    }
+
+    public static Courier courierWithoutLogin(){
+        return new Courier("", "aue-chelyaba", "Найтли");
+    }
+
+
+
+    public static Courier courierWirhoutPassword(){
+        return new Courier("lovenwantity", "", "Уоттакуот");
+    }
+}
